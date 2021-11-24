a6de207877faf472a50b31a15693cfbead3ca46b
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-11-24 13:51:25 +0200

    update the file structure

---
+++ server/src/database/config/fakeData.sql
@@ -0,0 +1,44 @@
+INSERT INTO users (name,email,password,identity_number)
+VALUES (
+    'Nadia',
+    'Nadia@gmail.com',
+    '',
+    '411939494'
+),
+(
+    'Mohammad',
+    'Mohammad@gmail.com',
+    '',
+    '411939494'
+),
+(
+    'Noor',
+    'Noor@gmail.com',
+    '',
+    '411939494'
+),
+(
+    'Elham',
+    'Elham@gmail.com',
+    '',
+    '411939494'
+),
+(
+    'Ibrahim',
+    'Ibrahim@gmail.com',
+    '',
+    '411939494'
+);
+INSERT INTO doctors (
+    name, email,password,specialty,center_name,clinic_location,phone_number,education , experience,treatments,image
+)
+VALUES (
+    'Dr. Kader Kaplan Mutluer',
+    'Dr.Mutluer@gmail.com',
+    '',
+    'Family Medicine',
+    'The International Medical Centre',
+    'downton',
+    '0598756049',
+    '['Master in Medical Science, VU University,Netherlands','Master in Medical Science, VU University,Netherlands']',
+)
