725c0d96e1bb8036756fb2f6007711f45a823055
mohammed Rayyes <mod.rayyes@gmail.com>
2021-11-26 23:41:40 +0200

    edit check email query

--- server/database/queries/checkEmail.js
+++ server/database/queries/checkEmail.js
@@ -1,4 +1,4 @@
 const connection = require('../connection');
 
-const checkEmail = (email) => connection.query('SELECT id from users where email=$1', [email]);
+const checkEmail = (email) => connection.query('SELECT id from users where email=$1 UNION SELECT id from doctors where email=$1', [email]);
 module.exports = checkEmail;
