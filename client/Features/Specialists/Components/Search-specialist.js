8963b3cfd0a8e1d7206333d25c99556a2a8dcdbc
Your Name <noorin.sa.99@gmail.com>
2021-12-07 13:28:41 +0200

    create specialist card Relates #53

--- client/Features/Specialists/Components/Search-specialist.js
+++ client/Features/Specialists/Components/Search-specialist.js
@@ -4,13 +4,15 @@ import { Searchbar } from 'react-native-paper';
 const SearchSpecialist = () => {
   const [searchQuery, setSearchQuery] = React.useState('');
 
-  const onChangeSearch = query => setSearchQuery(query);
+  const onChangeSearch = (query) => setSearchQuery(query);
 
   return (
     <Searchbar
-      placeholder="Search"
+      placeholder="Search doctors"
       onChangeText={onChangeSearch}
       value={searchQuery}
+      style={{ width: '70%', height: 35, borderRadius: 20, fontSize: 10 }}
+      iconColor="#022752"
     />
   );
 };
--- client/Features/Specialists/Screen/Specialist-view.js
+++ client/Features/Specialists/Screen/Specialist-view.js
@@ -1,82 +1,160 @@
-import  React, {useState, useEffect} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Platform,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
 import { Avatar, Button } from 'react-native-paper';
 import axioscreate from '../../../components/axioscreate';
 import SearchSpecialist from '../Components/Search-specialist';
 
-
 const SpecialistCard = () => {
   const [specialists, setSpecialists] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('There is no events with this filter');
+  const [errorMessage, setErrorMessage] = useState(
+    'There is no events with this filter',
+  );
 
-  const getSpecialists =async () => {
-    try{
+  const getSpecialists = async () => {
+    try {
       const { data } = await axioscreate.get('/api/v1/specialists');
       setSpecialists(data);
-    }catch{
-      // setErrorMessage(err.status);
+    } catch {
+      setErrorMessage(err.status);
     }
-  }
-  
-  useEffect( () => {
-      getSpecialists();
+  };
+
+  useEffect(() => {
+    getSpecialists();
   }, []);
 
-  return(
-    <>
-    <SearchSpecialist />
-    <View style={styles.container} >
-      <Text>{JSON.stringify(specialists)}</Text>
-    {specialists.length ? specialists.map((specialist) => {
-      return(
-        <View style={styles.cardContainer} key={specialist.id}>
-          <View style={styles.avatarContainer}>
-          <Avatar.Image size={65} source={specialist.image} />
-          </View>
-          <View style={styles.doctorContainer}>
-            <Text>{specialist.name}</Text>
-            <Text>{specialist.specialty}</Text>
-          </View>
-          <View style={styles.buttonsContainer}>
-            <Button icon="camera" mode="outlined" color="#FFB803"> View Profile</Button>
-            <Button icon="camera" mode="contained" color="#FFB803">
-        Book Now
-      </Button>
-          </View>
-        </View>
-       )
-    }): <Text>Hello</Text>}
-    </View>
-  </>
-  )
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <SearchSpecialist/>
+        {specialists.length ? (
+          specialists.map((specialist) => {
+            return (
+              <View style={styles.cardContainer} key={specialist.id}>
+                <View style={styles.doctorContainer}>
+                  <View style={styles.avatarContainer}>
+                    <Avatar.Image
+                      size={65}
+                      source={{ uri: specialist.image }}
+                    />
+                  </View>
+                  <View>
+                    <Text style={styles.headerFont}>{specialist.name}</Text>
+                    <Text style={styles.specialityFont}>
+                      {specialist.specialty}
+                    </Text>
+                  </View>
+                </View>
+                <View style={styles.buttonsContainer}>
+                  <TouchableOpacity>
+                    <Button
+                      style={styles.buttons}
+                      icon="account-outline"
+                      mode="outlined"
+                      color="#022752"
+                    >
+                      <Text style={styles.buttonsFont}> View Profile</Text>
+                    </Button>
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                  <Button
+                    style={styles.buttons}
+                    icon="calendar"
+                    mode="contained"
+                    color="#FFB803"
+                  >
+                    <Text style={styles.buttons}>Book Now</Text>
+                  </Button>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            );
+          })
+        ) : (
+          <Text>Hello</Text>
+        )}
+      </View>
+    </ScrollView>
+  );
 };
 
-  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 70,
-    backgroundColor:'#fffab1',
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    alignItems: 'center',
+  },
+  search: {
+    width:'50%',
+    backgroundColor:'green'
+
   },
   cardContainer: {
-    flex: 0.2,
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#87ceeb',
-    // justifyContent: 'space-around',
+    justifyContent: 'space-between',
+    borderRadius: 1,
+    marginTop: 20,
+    width: '90%',
+    shadowColor: 'rgba(0,0,0,0.3)',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1,
+
+    elevation: 1,
   },
-  avatarContainer:{
-    padding: 10
-    // backgroundColor: 'white',
+  avatarContainer: {
+    padding: 10,
   },
   doctorContainer: {
-    padding: 10
-    // backgroundColor: 'blue',
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '57%',
   },
   buttonsContainer: {
     padding: 10,
-    // backgroundColor: 'green',
-  }
+    width: '50%',
+  },
+  buttons: {
+    width: '80%',
+    marginTop: 5,
+    height: 30,
+    fontSize: 10,
+    fontWeight: 'bold',
+    shadowColor: 'rgba(0,0,0,0.1)',
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1,
+
+    elevation: 1,
+  },
+  headerFont: {
+    fontWeight: 'bold',
+    color: '#022752',
+  },
+  buttonsFont: {
+    fontSize: 9,
+    fontWeight: 'bold',
+  },
+  specialityFont: {
+    fontSize: 10,
+    fontWeight: '100',
+  },
 });
 
 export default SpecialistCard;
