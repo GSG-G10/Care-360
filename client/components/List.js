a9c99d0217215c68d8c0677370c33a801f757aee
divluffy <div.jo2022@gmail.com>
2021-12-09 15:14:27 +0200

    how

--- client/components/List.js
+++ client/components/List.js
@@ -1,40 +1,29 @@
 import * as React from 'react';
 import { List } from 'react-native-paper';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-const ListDoctors = ({ title, icon, details }) => {
-  const [expanded, setExpanded] = React.useState(true);
-
-  const handlePress = () => setExpanded(!expanded);
-  
-  const myArray = details ? details.split(".") : [] ;
-  
+const ListDoctors = ({ details }) => {
+  const myArray = details ? details.split('.') : [];
+  myArray.pop();
   return (
-    <List.Accordion
-      style={styles.listContainer}
-      title={title}
-      titleStyle={{ color: '#022752', fontWeight: 'bold', fontSize: 19 }}
-      left={(props) => <List.Icon {...props} icon={icon} />}
-      onPress={handlePress}
-    >
-      <List.Item title={myArray[0]} />
-      <List.Item title={myArray[1]} />
-      <List.Item title={myArray[2]} />
-    </List.Accordion>
+    <View>
+      {myArray.map((item, i) => {
+        return (
+          <List.Item
+            title={item}
+            key={i}
+            left={(props) => <List.Icon {...props} icon="circle-medium" />}
+          />
+        );
+      })}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  listContainer: {
-    backgroundColor: '#DCE4E8',
-    width: 350,
-    height: 55,
-    justifyContent: 'center',
-    borderRadius: 5,
+  points: {
+    fontSize: 2,
   },
-  details: {
-      fontSize:2,
-  }
 });
 
 export default ListDoctors;
